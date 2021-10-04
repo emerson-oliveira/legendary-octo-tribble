477b206193bb25c8d6e94e070f93c4a10a59c087
Emerson Silva <emerson-ods@hotmail.com>
2021-10-04 20:54:18 -0300

    feat: added user listing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,74 @@
-import type { NextPage } from 'next'
+import { useState } from 'react';
+import type { NextPage, GetStaticProps } from 'next'
+import Api from '../services/api';
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  Modal,
+  Button,
+} from 'react-bootstrap';
 
-const Home: NextPage = () => {
+interface HomeProps{
+  users: Array<User>;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+}
+
+const Home: NextPage <HomeProps> = ({users}) => {
+  const [numberUsers, setNumberUsers] = useState<number>(3);
+
+  const handlerNumberUsers = () => {
+    setNumberUsers(numberUsers + 3);
+  };
+  
   return (
-    <>Hello World</>
+    <Container>
+    <Row>
+      <Col>
+        <ListGroup>
+          {users.map((user, index) => (index < numberUsers ? (
+            <ListGroup.Item key={user.id}>
+                {user.name}
+            </ListGroup.Item>
+          ) : (
+            ''
+          )))}
+        </ListGroup>
+
+        <Button onClick={handlerNumberUsers}>mostrar mais</Button>
+      </Col>
+    </Row>
+  </Container>
   )
 }
 
 export default Home
+
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await Api.get('/users');
+  const users = data;
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
