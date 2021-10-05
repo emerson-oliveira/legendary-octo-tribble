7fd1e72ad2b032f48ad02aa15d4841e1d786200a
Emerson Silva <emerson-ods@hotmail.com>
2021-10-04 21:07:50 -0300

    feat: added centralized modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
-import type { NextPage, GetStaticProps } from 'next'
-import Api from '../services/api';
-import {
-  Container,
-  Row,
-  Col,
-  ListGroup,
-  Modal,
-  Button,
-} from 'react-bootstrap';
+import { useState } from "react";
+import type { NextPage, GetStaticProps } from "next";
+import Api from "../services/api";
+import { Container, Row, Col, ListGroup, Modal, Button } from "react-bootstrap";
 
-interface HomeProps{
+interface HomeProps {
   users: Array<User>;
 }
 
@@ -31,39 +24,61 @@ interface User {
   };
 }
 
-const Home: NextPage <HomeProps> = ({users}) => {
+const Home: NextPage<HomeProps> = ({ users }) => {
   const [numberUsers, setNumberUsers] = useState<number>(3);
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handlerModal = () => {
+    setShow(!show);
+  };
 
   const handlerNumberUsers = () => {
     setNumberUsers(numberUsers + 3);
   };
-  
+
   return (
     <Container>
-    <Row>
-      <Col>
-        <ListGroup>
-          {users.map((user, index) => (index < numberUsers ? (
-            <ListGroup.Item key={user.id}>
-                {user.name}
-            </ListGroup.Item>
-          ) : (
-            ''
-          )))}
-        </ListGroup>
+      <Row>
+        <Col>
+          <ListGroup>
+            {users.map((user, index) =>
+              index < numberUsers ? (
+                <ListGroup.Item key={user.id}>
+                  <Button variant="link" onClick={handlerModal}>
+                    {user.name}
+                  </Button>
+                </ListGroup.Item>
+              ) : (
+                ""
+              )
+            )}
+          </ListGroup>
 
-        <Button onClick={handlerNumberUsers}>mostrar mais</Button>
-      </Col>
-    </Row>
-  </Container>
-  )
-}
+          <Button onClick={handlerNumberUsers}>mostrar mais</Button>
+        </Col>
+      </Row>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Lorem ipsum dolor sit amet</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis
+          tortor sit amet ipsum imperdiet accumsan a nec nunc. Vestibulum ac
+          arcu id eros laoreet molestie nec nec mi.
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
 
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { data } = await Api.get('/users');
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await Api.get("/users");
   const users = data;
 
   return {
